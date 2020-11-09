0812bc2439f992e14c6369efe06d1428fb37b3d0
Priscila Villarreal <62452087+PriscilaVillarreal@users.noreply.github.com>
2020-11-09 13:38:25 -0600

    Corrección Usuario

--- WebAppBD/Models/Usuario.cs
+++ WebAppBD/Models/Usuario.cs
@@ -8,26 +8,39 @@ namespace WebAppBD.Models
     //[Table("Usuario", Schema = "bdo")]
     public partial class Usuario
     {
+        [Required(ErrorMessage = "Debe digitar el ID " +
+        "de Usuario")]
         public int IdUsuario { get; set; }
 
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Nombre")]
         public string Nombre { get; set; }
-        [Required(ErrorMessage = "Debe digitar el nombre ")]
+
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Apellido")]
         public string Apellido1 { get; set; }
-        [Required(ErrorMessage = "Debe digitar el apellido 1 ")]
        
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Debe digitar la " +
+        "Contraseña")]
         public string Password { get; set; }
-        [Required(ErrorMessage = "Debe digitar la contraseña ")]
+
+        [Required(ErrorMessage = "Debe digitar el " + "E-mail")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail no válido")]
         public string Correo { get; set; }
-        [Required(ErrorMessage = "Debe digitar el correo ")]
+
+        [Required(ErrorMessage = "Debe digitar la " +
+        "Cédula")]
         public int Cedula { get; set; }
-        [Required(ErrorMessage = "Debe digitar la cedula ")]
 
+        [Required(ErrorMessage = "Debe digitar el ID " +
+        "de Ficha Socio")]
         public int? IdRol { get; set; }
-        [Required(ErrorMessage = "Debe digitar el Rol")]
 
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Estado")]
         public string Estado { get; set; }
-        [Required(ErrorMessage = "Debe digitar el estado ")]
+        
 
         public virtual Roles IdRolNavigation { get; set; }
     }
