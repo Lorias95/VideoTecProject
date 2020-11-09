d48123979c18da0d34644fadd1affaa123496bc2
Priscila Villarreal <62452087+PriscilaVillarreal@users.noreply.github.com>
2020-11-09 16:53:15 -0600

    Validaciones para ArchiPeliPorTitulo

--- WebAppBD/Models/ArchiPeliPorTitulo.cs
+++ WebAppBD/Models/ArchiPeliPorTitulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebAppBD.Models
 {
@@ -10,12 +11,32 @@ namespace WebAppBD.Models
             Ficha = new HashSet<Ficha>();
         }
 
+        [Required(ErrorMessage = "Debe digitar el " +
+        "ID de Película por Título")]
         public int IdPeliPorTitulo { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Título")]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar la " +
+        "Duración")]
         public TimeSpan Duracion { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Número de Cinta")]
         public int NumCintas { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Género")]
         public string Genero { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Actor")]
         public string Actores { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar el " +
+        "Director")]
         public string Directores { get; set; }
 
         public virtual ICollection<Ficha> Ficha { get; set; }
