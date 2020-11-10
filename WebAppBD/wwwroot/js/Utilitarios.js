7b12b632149f61da31ee18b2d4d3e191f58038e9
lxX-lorias-Xxl <oriasdiego34@gmail.com>
2020-11-10 10:59:29 -0600

    Sweet Alert

---
+++ WebAppBD/wwwroot/js/Utilitarios.js
@@ -0,0 +1,14 @@
+﻿function verModal(titulo, texto) {
+    return Swal.fire({
+        title: titulo,
+        text: texto,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'No',
+        confirmButtonText: 'Si',
+        timer: 8000,
+        closeOnConfirm: false
+    })
+}
